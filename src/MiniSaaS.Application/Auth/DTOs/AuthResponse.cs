00c647f09675e3dd8372130a81af02179b9de0b2
mostafaroute7-debug <mostafaroute7@gmail.com>
2026-08-17 20:43:04 +0300

    Test

--- src/MiniSaaS.Application/Auth/DTOs/AuthResponse.cs
+++ src/MiniSaaS.Application/Auth/DTOs/AuthResponse.cs
@@ -3,6 +3,7 @@ namespace MiniSaaS.Application.Auth.DTOs;
 
 public sealed class AuthResponse
 {
+
     public string AccessToken { get; init; } = string.Empty;
     public DateTime ExpiresAt { get; init; }
 }
